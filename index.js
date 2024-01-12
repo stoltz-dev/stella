c814a0e2c5cfb6828f714b16ac77b404a33e8dbc
SyntZ <83966759+syntz-dev@users.noreply.github.com>
2024-01-12 10:06:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import { HfInference } from 'https://cdn.jsdelivr.net/npm/@huggingface/inference@2.6.4/+esm';
-import { createRepo, commit, deleteRepo, listFiles } from "https://cdn.jsdelivr.net/npm/@huggingface/hub@0.12.3/+esm";
-
 const tx = document.getElementsByTagName("textarea");
 for (let i = 0; i < tx.length; i++) {
   tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
