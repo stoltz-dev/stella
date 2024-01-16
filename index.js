ccb8022ab92bc90472c5ef66a5055cc4c46d51bb
SyntZ <83966759+syntz-dev@users.noreply.github.com>
2024-01-16 15:24:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ window.onload = function(){
     chat_message = document.getElementById("message");
     chat_message.innerHTML = marked.parse(chat_message.textContent);
 }
+
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = (this.scrollHeight) + "px";
+}
